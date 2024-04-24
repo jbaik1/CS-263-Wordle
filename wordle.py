f05cb1d8ca483034184432a8683354edf733998f
Jinwoo Baik <jinwoobaik@wifi-131-179-44-185.host.ucla.edu>
2024-04-24 13:49:29 -0700

    converted into class, duplicate letters should be good now. maybe need to change dictionary because there are to many uncommon words that chatgpt doesn't know.

--- wordle.py
+++ wordle.py
@@ -1,42 +1,56 @@
 import random
+from collections import Counter
 
-def wordle_init():
-    word_list = open("words.txt", 'r')
-    words = word_list.read().split()
-    answer = random.choice(words)
-
-    return answer
-
-def output(guess_word, answer):
-    # given guess, return an array of 'white', 'yellow', and 'green' colors
-    # white is incorrect, replacing grey so that it doesn't overlap w green
-    if len(guess_word) != len(answer): # not 5 letters
-        return 'length error'
+class Wordle():
+    def __init__(self) -> None:
+        word_list = open("words.txt", 'r')
+        words = word_list.read().split()
+        self.answer = random.choice(words)
     
-    out = []
-    # todo: handle logic for multiple yellows
-    for i in range(len(answer)):
-        if guess_word[i] == answer[i]:
-            out.append('G')
-        elif guess_word[i] != answer and guess_word[i] in answer: # letter in word but misplaced
-            out.append('Y')
-        else:
-            out.append('W')
-    return out
+    def get_answer(self):
+        return self.answer
+
+    def set_answer(self, new_answer):
+        self.answer = new_answer
+
+    def turn(self, guess):
+        # given guess, return an array of 'white', 'yellow', and 'green' colors
+        # white is incorrect, replacing grey so that it doesn't overlap w green
+        answer = self.answer
+
+        if len(guess) != len(answer): # not 5 letters
+            return 'length error'
+        
+        letter_count = Counter(answer)
+        
+        out = []
+        # todo: handle logic for multiple yellows
+        for i in range(len(answer)):
+            letter_guess = guess[i]
+            letter_answer = answer[i]
+
+            if letter_guess == letter_answer:
+                letter_count[letter_answer] -= 1
+                out.append('G')
+            elif letter_guess == letter_answer and letter_guess[i] in answer: # letter in word but misplaced
+                letter_count[letter_answer] -= 1
+                out.append('Y')
+            else:
+                out.append('W')
+        return out
 
 if __name__ == "__main__":
-    print('not functioning correctly yet')
-    answer = wordle_init()
+    wordle = Wordle()
     max_guess_count = 5
     
     #todo: api for input
     for i in range(max_guess_count):
         guess = input('guess '+ str(i)+'\n')
-        o = output(guess, answer)
+        o = wordle.turn(guess)
         print(o)
         if o == ['G', 'G', 'G', 'G', 'G']:
             print('correct guess!')
             break
-    print("couldn't get it in 5 guesses, answer was:", answer)
+    print("couldn't get it in 5 guesses, answer was:", wordle.get_answer())
 
 
