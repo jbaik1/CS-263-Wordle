7230666b5986b029e4077ff51e35b4b540451d35
lucasbandarkar@ucla.edu <lucasbandarkar@pluslab02-a6000.cs.ucla.edu>
2024-05-17 15:44:04 -0700

    added code to eval.py to process conversation object

--- eval.py
+++ eval.py
@@ -1,3 +1,6 @@
+from wordle import Wordle
+import re
+
 # functions for evaluating performances
 #
 # input is for 1 "round". Can make it more general if necessary
@@ -47,10 +50,96 @@ def eval_avg_acc(guesses, answer):
 
 
 
-def eval_entropy(guesses=[], answer):
+def eval_entropy(answer, guesses=[]):
     # quality of guess using entropy
     # https://www.youtube.com/watch?v=v68zYyaEmEA
     # unlikely that the llm considers this 
     # maybe not worth it, given that it's kinda hard to program this
     
     return
+
+class Conversation():
+    def __init__(self, conversation) -> None:
+        self.raw_convo = conversation
+        self.length = len(self.raw_convo)
+        self.correct_answer = self.extract_correct_answer()
+        self.guesses = self.extract_guesses()
+
+        self.wordle = Wordle()
+        self.wordle.set_answer(self.correct_answer)
+        
+
+    def extract_correct_answer(self):
+        last = self.raw_convo[-1]['content'].split()
+        if last[-3:-1] == ['answer', 'is']:
+            return last[-1]
+        else:
+            raise("what other edge cases exist ?")
+
+    def extract_guesses(self):
+        guesses = []
+        still_in_prompt = True
+        for i in range(len(self.raw_convo)):
+            if still_in_prompt and (self.raw_convo[i]['role'] == 'system') and (self.raw_convo[i]['content'] == 'Now it is your turn'):
+                still_in_prompt = False
+                self.end_of_prompt = i
+            if not still_in_prompt and self.raw_convo[i]['role'] == 'assistant':
+                pattern = r'[A-Z]{5}'
+                guess = re.findall(pattern, self.raw_convo[i]['content'])[-1]
+                guesses.append(guess)
+        
+        if len(guesses) > 5:
+            raise("too many guesses")
+        return guesses
+        
+    
+
+
+def main():
+    example_convo = [
+        {'role': 'system', 'content': ('Wordle is a 5 letter word guessing game. Each letter in a guess will either be a correct letter in correct position, an invalid letter or a correct letter present in the word but in a wrong position. After each guess, we get a result',)},
+        {'role': 'system', 'content': 'Example #1:'},
+        {'role': 'user', 'content': 'Enter guess #1'},
+        {'role': 'assistant', 'content': 'BAGEL'},
+        {'role': 'user', 'content': 'Letter B is not anywhere present in the word, Letter A is not anywhere present in the word, Letter G is not anywhere present in the word, Letter E at Position 3 is in the word and at the correct position, Letter L is not anywhere present in the word, '},
+        {'role': 'user', 'content': 'Enter guess #2'},
+        {'role': 'assistant', 'content': 'COVER'},
+        {'role': 'user', 'content': 'Letter C is not anywhere present in the word, Letter O is not anywhere present in the word, Letter V is not anywhere present in the word, Letter E at Position 3 is in the word and at the correct position, Letter R at Position 4 is in the wrong position but is in the word, '},
+        {'role': 'user', 'content': 'Enter guess #3'},
+        {'role': 'assistant', 'content': 'THREE'},
+        {'role': 'user', 'content': 'Congrats you found the correct word THREE in 3 guesses'},
+        {'role': 'system', 'content': 'Example #1:'},
+        {'role': 'user', 'content': 'Enter guess #1'},
+        {'role': 'assistant', 'content': 'WHILE'},
+        {'role': 'user', 'content': 'Letter W is not anywhere present in the word, Letter H is not anywhere present in the word, Letter I is not anywhere present in the word, Letter L is not anywhere present in the word, Letter E is not anywhere present in the word, '},
+        {'role': 'user', 'content': 'Enter guess #2'},
+        {'role': 'assistant', 'content': 'RADIO'},
+        {'role': 'user', 'content': 'Letter R is not anywhere present in the word, Letter A at Position 1 is in the wrong position but is in the word, Letter D is not anywhere present in the word, Letter I is not anywhere present in the word, Letter O is not anywhere present in the word, '},
+        {'role': 'user', 'content': 'Enter guess #3'},
+        {'role': 'assistant', 'content': 'START'},
+        {'role': 'user', 'content': 'Letter S at Position 0 is in the word and at the correct position, Letter T at Position 1 is in the word and at the correct position, Letter A at Position 2 is in the word and at the correct position, Letter R is not anywhere present in the word, Letter T is not anywhere present in the word, '},
+        {'role': 'user', 'content': 'Enter guess #4'},
+        {'role': 'assistant', 'content': 'STAGE'},
+        {'role': 'user', 'content': 'Letter S at Position 0 is in the word and at the correct position, Letter T at Position 1 is in the word and at the correct position, Letter A at Position 2 is in the word and at the correct position, Letter G is not anywhere present in the word, Letter E is not anywhere present in the word, '},
+        {'role': 'user', 'content': 'Enter guess #5'},
+        {'role': 'assistant', 'content': 'STAFF'},
+        {'role': 'user', 'content': 'Congrats you found the correct word STAFF in 5 guesses'},
+        {'role': 'system', 'content': 'Now it is your turn'},
+        {'role': 'user', 'content': 'Enter guess #1'},
+        {'role': 'assistant', 'content': 'BAGEL'},
+        {'role': 'user', 'content': 'Letter B is not anywhere present in the word, Letter A at Position 1 is in the word and at the correct position, Letter G is not anywhere present in the word, Letter E at Position 3 is in the wrong position but is in the word, Letter L is not anywhere present in the word, '},
+        {'role': 'assistant', 'content': 'BAGEL'},
+        {'role': 'user', 'content': 'Letter B is not anywhere present in the word, Letter A at Position 1 is in the word and at the correct position, Letter G is not anywhere present in the word, Letter E at Position 3 is in the wrong position but is in the word, Letter L is not anywhere present in the word, '},
+        {'role': 'assistant', 'content': 'COVER'},
+        {'role': 'user', 'content': 'Letter C is not anywhere present in the word, Letter O is not anywhere present in the word, Letter V is not anywhere present in the word, Letter E at Position 3 is in the wrong position but is in the word, Letter R at Position 4 is in the wrong position but is in the word, '},
+        {'role': 'assistant', 'content': 'COVER'},
+        {'role': 'user', 'content': 'Letter C is not anywhere present in the word, Letter O is not anywhere present in the word, Letter V is not anywhere present in the word, Letter E at Position 3 is in the wrong position but is in the word, Letter R at Position 4 is in the wrong position but is in the word, '},
+        {'role': 'assistant', 'content': 'THREE'},
+        {'role': 'user', 'content': 'Letter T is not anywhere present in the word, Letter H is not anywhere present in the word, Letter R at Position 2 is in the wrong position but is in the word, Letter E at Position 3 is in the wrong position but is in the word, Letter E at Position 4 is in the word and at the correct position, '},
+        {'role': 'system', 'content': '5 guesses exhausted! The correct answer is raise'}
+    ]
+    c = Conversation(example_convo)
+    print(c.correct_answer, c.guesses)
+
+if __name__ == "__main__":
+    main()
